46f9a23c056215bad75d12bcfd272780203f1571
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2020-12-16 15:29:33 -0600

    Innit repo

---
+++ sketch_dec16a/sketch_dec16a.ino
@@ -0,0 +1,26 @@
+/**********************************************************************************************
+ *                 Protección de un cuarto con Arduino MEGA, RFID y passcode
+ *
+ * Proyecto creado por:
+ *                      Nicole Rodríguez González
+ *                      Víctor Hugo Vázquez Gómez
+***********************************************************************************************/
+// Librerías usadas
+#include <Servo.h>
+#include <MFRC522.h>
+#include <deprecated.h>
+#include <MFRC522Extended.h>
+#include <require_cpp11.h>
+#include <Key.h>
+#include <Keypad.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
